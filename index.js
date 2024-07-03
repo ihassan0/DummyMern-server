72f7c23d50e421c8d1d1303278cff13ce53fb72f
ihassan0 <hassanbabar852629@gmail.com>
2024-07-02 22:40:22 -0700

    updated.

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-app.use(cors());
-app.use(express.json());
+app.use(cors(
+  // {
+  //   origin; [],
+  //   methods:  ["GET","POST","PUT","DELETE"],
+  //   credentials:true,
+  // }
+));
+app.use(bodyParser.json()); app.use(bodyParser.urlencoded({ extended: true }));
 
 const mongoose = require('mongoose')
 const FoodItem = require('./models/FoodItem')
