7dc7182e529b2914af9c6e261dfc9cf578b6b7ae
ihassan0 <hassanbabar852629@gmail.com>
2024-07-03 01:16:05 -0700

    update 10.

--- index.js
+++ index.js
@@ -1,35 +1,39 @@
-const express = require('express');
-const app = express();
-const cors = require('cors');
-const mongoose = require('mongoose');
-const FoodItem = require('../models/FoodItem');
-const FoodCategory = require('../models/FoodCategory');
-const createUserRoute = require('../routes/CreateUser');
-
-app.use(cors());
+const express = require('express')
+const app = express()
+const cors = require('cors')
+app.use(cors(
+  // {
+  //   origin; [],
+  //   methods:  ["GET","POST","PUT","DELETE"],
+  //   credentials:true,
+  // }
+));
 app.use(express.json());
 
-mongoose.connect("mongodb+srv://rooty:root1122@cluster1.izvgr4k.mongodb.net/gofood?retryWrites=true&w=majority&appName=Cluster1", { useNewUrlParser: true, useUnifiedTopology: true });
-console.log("connected");
+const mongoose = require('mongoose')
+const FoodItem = require('../models/FoodItem')
+const FoodCategory = require('../models/FoodCategory')
+ 
+ mongoose.connect("mongodb+srv://rooty:root1122@cluster1.izvgr4k.mongodb.net/gofood?retryWrites=true&w=majority&appName=Cluster1")
+  console.log("connected")
 
-app.get('/', (req, res) => {
-  res.send('Hello world');
-});
-
-app.post('/foods', (req, res) => {
-  Promise.all([
-    FoodItem.find().exec(),
-    FoodCategory.find().exec()
+  app.get('/', (req, res) => {
+    res.send('Hello world')
+  })
+  app.post('/foods', (req, res) => {
+    Promise.all([
+      FoodItem.find().exec(),
+      FoodCategory.find().exec()
   ])
   .then(([foodItems, foodCategories]) => {
-    res.json({ foodItems, foodCategories });
+      res.json({ foodItems, foodCategories });
   })
-  .catch(err => {
-    res.status(500).json({ error: err.message });
-  });
-});
 
-app.use('/api', createUserRoute);
+  })
 
-// Export the Express app as a handler for Vercel
-module.exports = app;
+  app.use('/api/', require('../routes/CreateUser'))
+
+
+app.listen(4000, () => {
+    console.log('listening to port')
+}) 
